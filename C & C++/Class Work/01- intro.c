833a824b0326f865d78946b4eee4325f989504d1
Dhruv6234 <prajapatidhruv6234@gmail.com>
2023-07-17 10:49:41 +0530

    done

---
+++ C & C++/Class Work/01- intro.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+main(){
+	printf("hello world Dhruv and he is a %d",420);
+}
+
+//Notes 
+
+//# is a pre processor
+//stdio standard input output
+//.h header file
+//this whole is a library
+
+//in built and user defined
+
+//main is a user defined function
